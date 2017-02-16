9e366441d9f12e085076d33bf98f3091c1ed1100
Carole <carole.velarde@orange.fr>
2017-02-16 09:17:11 +0100

    un peu de Javascript

---
+++ forminscrip.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	console.log("coucou")
+	//alert("Bonjour et bienvenue!")
+	$("form").submit(function(event){
+         event.preventDefault();
+         console.log("send");
+         var nom = $("#nom").val()
+         var prenom = $("#prenom").val()
+         var login = $("#login").val()
+         var email = $("#email").val()
+         var ville = $ ("#ville").val()
+
+         localStorage.setItem("")
+
+
+
+	})
+
+});
